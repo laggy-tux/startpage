dbc39efef13017727d6ced36fc775c98da00c5cf
Joy Yahshua <85402808+laggy-tux@users.noreply.github.com>
2024-08-04 13:20:59 +0530

    Update tabs.component.js

--- src/components/tabs/tabs.component.js
+++ src/components/tabs/tabs.component.js
@@ -180,7 +180,7 @@ class Tabs extends Component {
       }
 
       .categories ul .links a {
-          color: #f5e0dc;
+          color: #cdd6f4;
           text-decoration: none;
           font: 700 18px 'Roboto', sans-serif;
           transition: all .2s;
@@ -239,7 +239,7 @@ class Tabs extends Component {
       }
 
       .categories .links li h1 {
-          color: #f5e0dc;
+          color: #cdd6f4;
 	        opacity: 0.5;
           font-size: 13px;
           margin-bottom: 1em;
