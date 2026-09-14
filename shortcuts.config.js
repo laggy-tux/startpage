b4cfecfc049aa3269fb9d3beaee15e5311c64307
Joy Yahshua <85402808+laggy-tux@users.noreply.github.com>
2026-09-14 11:01:56 +0530

    Update Anime URL in shortcuts.config.js

--- shortcuts.config.js
+++ shortcuts.config.js
@@ -32,7 +32,7 @@ window.SHORTCUTS_CONFIG = [
   {
     id: 'anime',
     name: 'Anime',
-    url: 'https://anikototv.to/',
+    url: 'https://www.crunchyroll.com/discover/',
     icon: `<svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12zM10 8v8l6-4-6-4z"/></svg>`
   },
   {
